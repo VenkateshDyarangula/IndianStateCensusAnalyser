0c8d8049e835e0e1ba7c254edd3b6e1612b0a186
venkatesh179 <venkateshdyarangula@gmail.com>
2021-04-22 12:06:49 +0530

    Added

---
+++ IndianStateConsusAnalyser/CensusAnalyser.cs
@@ -0,0 +1,27 @@
+﻿using IndianStateConsusAnalyser.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStateConsusAnalyser
+{
+
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA, US, BRAZIL
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+        public Dictionary<string, CensusDTO> LoadCensusData(string csvFilePath, Country country, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+    }
+
+
+
+}
